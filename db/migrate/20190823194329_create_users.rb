50722281fc65d11755aaf72ab5f4d8a92324cd39
Savannah C <sgcorrell@gmail.com>
2019-08-23 19:44:03 +0000

    Automatically backed up by Learn

--- db/migrate/20190823194329_create_users.rb
+++ db/migrate/20190823194329_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string
+      t.string :email
+      t.string :image
+      t.string :uid
     end
   end
 end
